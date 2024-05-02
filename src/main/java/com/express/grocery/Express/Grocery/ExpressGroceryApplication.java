169b1e125dd63d96a0755cbbc461cc2f7727ce4c
vasu.aggarwal <vasu.aggarwal@extramarks.com>
2024-05-02 11:15:57 +0530

    Initial Commit

---
+++ src/main/java/com/express/grocery/Express/Grocery/ExpressGroceryApplication.java
@@ -0,0 +1,13 @@
+package com.express.grocery.Express.Grocery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExpressGroceryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExpressGroceryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/express/grocery/Express/Grocery/ExpressGroceryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.express.grocery.Express.Grocery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpressGroceryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
