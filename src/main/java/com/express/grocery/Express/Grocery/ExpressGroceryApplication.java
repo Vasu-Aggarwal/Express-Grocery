f85d3422e3a2dffd2af1f693cfbd5b8b33962684
Vasu Aggarwal <svasu.sv95@gmail.com>
2024-06-02 23:15:47 +0530

    Jar file

--- src/main/java/com/express/grocery/Express/Grocery/ExpressGroceryApplication.java
+++ src/main/java/com/express/grocery/Express/Grocery/ExpressGroceryApplication.java
@@ -2,7 +2,9 @@ package com.express.grocery.Express.Grocery;
 
 import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 import java.util.TimeZone;
